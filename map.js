9a81c233e4a55adae01435dd6a8a87d342e63b6b
nstewart23 <nstewart23@Juno.com>
2026-03-05 15:26:31 -0600

    Add files via upload

---
+++ map.js
@@ -0,0 +1,25 @@
+var map = L.map('map').setView([39.5,-98.35],4);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
+maxZoom:19
+}).addTo(map);
+
+var markers=[];
+
+fetch("tracks.json")
+.then(response=>response.json())
+.then(data=>{
+
+data.forEach(track=>{
+
+var marker=L.marker([track.lat,track.lon]).addTo(map);
+
+marker.bindPopup(
+"<b>"+track.name+"</b><br>"+
+track.city+", "+track.state+"<br>"+
+"<a href='"+track.url+"' target='_blank'>Track Website</a>"
+);
+
+});
+
+});
